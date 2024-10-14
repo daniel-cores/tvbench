fd86e7888ff9e5ef87240330904fe8a62035d722
Daniel Cores <daniel.cores@usc.es>
2024-10-14 23:14:14 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+<div align="center">
+
+<h2><a href="https://github.com/daniel-cores/tvbench">TVBench: Redesigning Video-Language Evaluation</a></h2>
+
+[Daniel Cores](https://scholar.google.com/citations?user=pJqkUWgAAAAJ)\*,
+[Michael Dorkenwald](https://scholar.google.com/citations?user=KY5nvLUAAAAJ)\*,
+[Manuel Mucientes](https://scholar.google.com.vn/citations?user=raiz6p4AAAAJ),
+[Cees G. M. Snoek](https://scholar.google.com/citations?user=0uKdbscAAAAJ),
+[Yuki M. Asano](https://scholar.google.co.uk/citations?user=CdpLhlgAAAAJ)
+
+*Equal contribution.
+
+[![arXiv](https://img.shields.io/badge/cs.CV-2410.07752-b31b1b?logo=arxiv&logoColor=red)](https://arxiv.org/abs/2410.07752)
+[![Dataset meta](https://img.shields.io/badge/%F0%9F%A4%97%20Dataset-TVBench-blue)](https://huggingface.co/datasets/FunAILab/TVBench) 
+</div>
+
+This is the official code repository for "TVBench: Redeisgning Video-Language Evaluation".
+
+# TVBench
+TVBench is a new benchmark specifically created to evaluate temporal understanding in video QA. We identified three main issues in existing datasets: (i) static information from single frames is often sufficient to solve the tasks (ii) the text of the questions and candidate answers is overly informative, allowing models to answer correctly without relying on any visual input (iii) world knowledge alone can answer many of the questions, making the benchmarks a test of knowledge replication rather than visual reasoning. In addition, we found that open-ended question-answering benchmarks for video understanding suffer from similar issues while the automatic evaluation process with LLMs is unreliable, making it an unsuitable alternative.
+
+We defined 10 temporally challenging tasks that either require repetition counting (Action Count), properties about moving objects (Object Shuffle, Object Count, Moving Direction), temporal localization (Action Localization, Unexpected Action), temporal sequential ordering (Action Sequence, Scene Transition, Egocentric Sequence) and distinguishing between temporally hard Action Antonyms such as "Standing up" and "Sitting down".
+
+In TVBench, state-of-the-art text-only, image-based, and most video-language models perform close to random chance, with only the latest strong temporal models, such as Tarsier, outperforming the random baseline. In contrast to MVBench, the performance of these temporal models significantly drops when videos are reversed.
+
+![image](figs/fig1.png)
+
+### Dataset statistics:
+The table below shows the number of samples and the average frame length for each task in TVBench.
+
+<center>
+<img src="figs/tvbench_stats.png" alt="drawing" width="400"/>
+</center>
+
+## Download and evaluation
+TVBench can be downloaded from [here](https://huggingface.co/datasets/FunAILab/TVBench)🤗.
+
+[infer.ipynb](infer.ipynb) contains an example of how to evaluate [VideoChat2](https://github.com/OpenGVLab/Ask-Anything/tree/main/video_chat2) on TVBench. TVBench follows the same format as MVBench, so any codebase with support for MVBench can be directly adapted to TVBench by simple updating the dataset path.
+
+
+## Leaderboard
+![image](figs/sota.png)
+
+# Citation
+If you find this benchmark useful, please consider citing:
+```
+
+@misc{cores2024tvbench,
+  author = {Daniel Cores and Michael Dorkenwald and Manuel Mucientes and Cees G. M. Snoek and Yuki M. Asano},
+  title = {TVBench: Redesigning Video-Language Evaluation},
+  year = {2024},
+  eprint = {arXiv:2410.07752},
+}
+
+```
---
+++ vqa_model.py
@@ -0,0 +1,179 @@
+import torch
+import numpy as np
+
+from peft import get_peft_model, LoraConfig, TaskType
+from transformers import StoppingCriteria, StoppingCriteriaList
+
+from models.videochat2_it import VideoChat2_it
+from utils.config import Config
+
+def build_videoChat2_infer(model_path, cfg):
+    # load stage2 model
+    cfg.model.vision_encoder.num_frames = 4
+    model = VideoChat2_it(config=cfg.model)
+
+    model = model.to(torch.device(cfg.device))
+    model = model.eval()
+
+    # add lora to run stage3 model
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM, inference_mode=False, 
+        r=16, lora_alpha=32, lora_dropout=0.
+    )
+    model.llama_model = get_peft_model(model.llama_model, peft_config)
+
+    state_dict = torch.load(model_path, "cpu")
+
+    if 'model' in state_dict.keys():
+        msg = model.load_state_dict(state_dict['model'], strict=False)
+    else:
+        msg = model.load_state_dict(state_dict, strict=False)
+    print(msg)
+
+    model = model.eval()
+    return model
+
+def get_sinusoid_encoding_table(n_position=784, d_hid=1024, cur_frame=8, ckpt_num_frame=4, pre_n_position=784): 
+    ''' Sinusoid position encoding table ''' 
+    # TODO: make it with torch instead of numpy 
+    def get_position_angle_vec(position): 
+        return [position / np.power(10000, 2 * (hid_j // 2) / d_hid) for hid_j in range(d_hid)] 
+    
+    # generate checkpoint position embedding
+    sinusoid_table = np.array([get_position_angle_vec(pos_i) for pos_i in range(pre_n_position)]) 
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2]) # dim 2i 
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2]) # dim 2i+1 
+    sinusoid_table = torch.tensor(sinusoid_table, dtype=torch.float, requires_grad=False).unsqueeze(0)
+    
+    print(f"n_position: {n_position}")
+    print(f"pre_n_position: {pre_n_position}")
+    
+    if n_position != pre_n_position:
+        T = ckpt_num_frame # checkpoint frame
+        P = 14 # checkpoint size
+        C = d_hid
+        new_P = int((n_position // cur_frame) ** 0.5) # testing size
+        if new_P != 14:
+            print(f'Pretraining uses 14x14, but current version is {new_P}x{new_P}')
+            print(f'Interpolate the position embedding')
+            sinusoid_table = sinusoid_table.reshape(-1, T, P, P, C)
+            sinusoid_table = sinusoid_table.reshape(-1, P, P, C).permute(0, 3, 1, 2)
+            sinusoid_table = torch.nn.functional.interpolate(
+                sinusoid_table, size=(new_P, new_P), mode='bicubic', align_corners=False)
+            # BT, C, H, W -> BT, H, W, C ->  B, T, H, W, C
+            sinusoid_table = sinusoid_table.permute(0, 2, 3, 1).reshape(-1, T, new_P, new_P, C)
+            sinusoid_table = sinusoid_table.flatten(1, 3)  # B, THW, C
+    
+    if cur_frame != ckpt_num_frame:
+        print(f'Pretraining uses 4 frames, but current frame is {cur_frame}')
+        print(f'Interpolate the position embedding')
+        T = ckpt_num_frame # checkpoint frame
+        new_T = cur_frame # testing frame
+        # interpolate
+        P = int((n_position // cur_frame) ** 0.5) # testing size
+        C = d_hid
+        sinusoid_table = sinusoid_table.reshape(-1, T, P, P, C)
+        sinusoid_table = sinusoid_table.permute(0, 2, 3, 4, 1).reshape(-1, C, T)  # BHW, C, T
+        sinusoid_table = torch.nn.functional.interpolate(sinusoid_table, size=new_T, mode='linear')
+        sinusoid_table = sinusoid_table.reshape(1, P, P, C, new_T).permute(0, 4, 1, 2, 3) # B, T, H, W, C
+        sinusoid_table = sinusoid_table.flatten(1, 3)  # B, THW, C
+        
+    return sinusoid_table
+
+def get_prompt(conv):
+    ret = conv.system + conv.sep
+    for role, message in conv.messages:
+        if message:
+            ret += role + ": " + message + conv.sep
+        else:
+            ret += role + ":"
+    return ret
+
+
+def get_prompt2(conv):
+    ret = conv.system + conv.sep
+    count = 0
+    for role, message in conv.messages:
+        count += 1
+        if count == len(conv.messages):
+            ret += role + ": " + message
+        else:
+            if message:
+                ret += role + ": " + message + conv.sep
+            else:
+                ret += role + ":"
+    return ret
+
+
+def get_context_emb(conv, model, img_list, answer_prompt=None, print_res=False):
+    if answer_prompt:
+        prompt = get_prompt2(conv)
+    else:
+        prompt = get_prompt(conv)
+    if print_res:
+        print(prompt)
+    if '<VideoHere>' in prompt:
+        prompt_segs = prompt.split('<VideoHere>')
+    else:
+        prompt_segs = prompt.split('<ImageHere>')
+    assert len(prompt_segs) == len(img_list) + 1, "Unmatched numbers of image placeholders and images."
+    with torch.no_grad():
+        seg_tokens = [
+            model.llama_tokenizer(
+                seg, return_tensors="pt", add_special_tokens=i == 0).to("cuda:0").input_ids
+            # only add bos to the first seg
+            for i, seg in enumerate(prompt_segs)
+        ]
+        seg_embs = [model.llama_model.base_model.model.model.embed_tokens(seg_t) for seg_t in seg_tokens]
+    mixed_embs = [emb for pair in zip(seg_embs[:-1], img_list) for emb in pair] + [seg_embs[-1]]
+    mixed_embs = torch.cat(mixed_embs, dim=1)
+    return mixed_embs
+
+
+def ask(text, conv):
+    conv.messages.append([conv.roles[0], text + '\n'])
+        
+
+class StoppingCriteriaSub(StoppingCriteria):
+    def __init__(self, stops=[], encounters=1):
+        super().__init__()
+        self.stops = stops
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor):
+        for stop in self.stops:
+            if torch.all((stop == input_ids[0][-len(stop):])).item():
+                return True
+        return False
+    
+    
+def answer(conv, model, img_list, do_sample=True, max_new_tokens=200, num_beams=1, min_length=1, top_p=0.9,
+               repetition_penalty=1.0, length_penalty=1, temperature=1.0, answer_prompt=None, print_res=False):
+    stop_words_ids = [
+        torch.tensor([835]).to("cuda:0"),
+        torch.tensor([2277, 29937]).to("cuda:0")]  # '###' can be encoded in two different ways.
+    stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stops=stop_words_ids)])
+    
+    conv.messages.append([conv.roles[1], answer_prompt])
+    embs = get_context_emb(conv, model, img_list, answer_prompt=answer_prompt, print_res=print_res)
+    with torch.no_grad():
+        outputs = model.llama_model.generate(
+            inputs_embeds=embs,
+            max_new_tokens=max_new_tokens,
+            stopping_criteria=stopping_criteria,
+            num_beams=num_beams,
+            do_sample=do_sample,
+            min_length=min_length,
+            top_p=top_p,
+            repetition_penalty=repetition_penalty,
+            length_penalty=length_penalty,
+            temperature=temperature,
+        )
+    output_token = outputs[0]
+    if output_token[0] == 0:  # the model might output a unknow token <unk> at the beginning. remove it
+            output_token = output_token[1:]
+    if output_token[0] == 1:  # some users find that there is a start token <s> at the beginning. remove it
+            output_token = output_token[1:]
+    output_text = model.llama_tokenizer.decode(output_token, add_special_tokens=False)
+    output_text = output_text.split('###')[0]  # remove the stop sign '###'
+    output_text = output_text.split('Assistant:')[-1].strip()
+    conv.messages[-1][1] = output_text
+    return output_text, output_token.cpu().numpy()
